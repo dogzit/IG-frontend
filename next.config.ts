2aec92d20ad0f0dfdfe705ba120a2d32bc9796f8
25HP7881@nest.edu.mn <Zoloo@pinecones-iMac.local>
2025-11-04 17:55:53 +0800

    1

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   env: {
     HF: process.env.HF ?? "",
     BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN ?? "",
+    BLOB_SECRET_READ_WRITE_TOKEN:
+      process.env.BLOB_SECRET_READ_WRITE_TOKEN ?? "",
   },
 };
 
